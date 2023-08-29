6c0b6339c02a2d3bb3574e06292468ce27677610
srajan tiwari <tiwarisrajan07@gmail.com>
2023-08-29 05:10:32 -0700

    New file

---
+++ web.js
@@ -0,0 +1,26 @@
+const populate = async (value, currency) => {
+    let myStr = ""
+    let  url = "https://api.currencyapi.com/v3/latest?apikey=cur_live_YuTQJaCpVJJZcS7GyDdGWn1SAb0yDIjow1CgqOSK" + currency
+     let response = await fetch(url)
+    let rJson = await response.json()
+    document.querySelector(".output").style.display = "block"
+
+    for (let key of Object.keys(rJson["data"])) {
+        myStr += ` <tr>
+                        <td>${key}</td>
+                        <td>${rJson["data"][key]["code"]}</td>
+                        <td>${Math.round(rJson["data"][key]["value"] * value)}</td>
+                    </tr> 
+                `
+    }
+    const tableBody = document.querySelector("tbody");
+    tableBody.innerHTML = myStr;
+
+}
+const btn = document.querySelector("button")
+btn.addEventListener("click", (e) => {
+    e.preventDefault()
+    const value = parseInt(document.querySelector("input[name='quantity']").value);
+    const currency = document.querySelector("select[name='currency']").value
+    populate(value, currency)
+})
